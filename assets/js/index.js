f61d0d1fa35718e43165e4bf309ae2627328ae92
Katherine Moreno <kata_moreno@hotmail.com>
2023-08-19 18:07:03 -0500

    copy page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,31 +1,31 @@
 import axios from 'axios';
 
 document.querySelector(".reg").addEventListener("click", () => {
-    let n_celular = document.querySelector(".n_celular").value
-    let correo = document.querySelector(".correo").value
-    let password = document.querySelector(".password").value
-    if (n_celular == "" || correo == "" || password == "") {
+    let empresa = document.querySelector(".empresa").value
+    let nombre = document.querySelector(".nombre").value
+    let cargo = document.querySelector(".cargo").value
+    let celular = document.querySelector(".celular").value
+    let email = document.querySelector(".email").value
+    if (empresa == "" || nombre == "" || cargo == "" || celular == "" || email == "") {
         return alert("Todos los campos son obligatorios");
                 
     }
-    if (!esEmailValido(correo)) {
+    if (!esEmailValido(email)) {
         return alert("El correo no es válido");       
     }
-    if (password.length < 6) {
-        return alert("La contraseña debe ser mayor a 5 carácteres");
-    }
     let data = {
-        nombre_usuario: "",
-        clave : password,
-        correo_usuario: correo,
-        telefono_usuario: n_celular,
+        empresa,
+        nombre,
+        cargo,
+        celular,
+        email
     }
     register(data)
 })
 
 async function register(data){
     try {
-        let res = await axios.post("http://localhost:5000/api/v1/register", data)
+        let res = await axios.post("http://localhost:3000/registrate", data)
         console.log(res);
         return alert("Usuario registrado correctamente");
     } catch (error) {
